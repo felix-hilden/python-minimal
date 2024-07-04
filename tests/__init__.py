6c1cc2063b280c9bcefa63cc454f19eff5c97220
felix-hilden <felix.hilden@gmail.com>
2024-07-04 22:15:31 +0300

    Update template

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,10 @@ import unittest
 
 
 class TestPackage(unittest.TestCase):
-    pass
+    def test_package(self):
+        import package
+
+        self.assertEqual(package.__version__, '0.1.0')
 
 
 if __name__ == '__main__':
